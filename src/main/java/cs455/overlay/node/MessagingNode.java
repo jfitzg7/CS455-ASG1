a9e136d909620e645873d4002b9424082aa8d6de
Jack Fitzgerald <jhfitzg@rams.colostate.edu>
2020-01-25 21:59:59 -0700

    Messed around and got the MessagingNode and Registry to start talking to each other

--- src/main/java/cs455/overlay/node/MessagingNode.java
+++ src/main/java/cs455/overlay/node/MessagingNode.java
@@ -1,4 +1,25 @@
 package cs455.overlay.node;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.*;
+import cs455.overlay.transport.TCPSender;
+
 public class MessagingNode {
+
+    private static Logger LOG = LogManager.getLogger(MessagingNode.class);
+
+    public static void main(String[] args) {
+        byte[] message = {125, 40, 32, 47};
+        try {
+            Socket socket = new Socket("127.0.0.1", 5000);
+            TCPSender sender = new TCPSender(socket);
+            sender.sendData(message);
+            socket.close();
+        } catch (IOException ioe) {
+            LOG.error(ioe.getMessage());
+        }
+    }
 }
--- src/main/java/cs455/overlay/node/Registry.java
+++ src/main/java/cs455/overlay/node/Registry.java
@@ -1,4 +1,22 @@
 package cs455.overlay.node;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import cs455.overlay.transport.TCPServerThread;
+
+import java.io.IOException;
+
 public class Registry {
+
+    private static Logger LOG = LogManager.getLogger(Registry.class);
+
+    public static void main(String[] args) {
+        try {
+            TCPServerThread server = new TCPServerThread(5000);
+            LOG.info("Starting server thread...");
+            (new Thread(server)).start();
+        } catch (IOException ioe) {
+            LOG.error(ioe.getMessage());
+        }
+    }
 }
